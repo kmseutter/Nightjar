a2065450ed7e11bcae3447898d72a4259a494f3a
Jerry Seutter <jerry.seutter@mobia.io>
2026-07-31 21:55:32 -0600

    more code

--- src/run-api.py
+++ src/run-api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlmodel import Field, Session, SQLModel, create_engine
 import uvicorn
 
 app = FastAPI(title="User Auth API")
@@ -27,13 +28,15 @@ async def logout():
 
 
 class Settings(BaseSettings):
+    # Example for .env file: database_url=postgresql://user:password@10.0.0.66:5432/my_rbac_db
     database_url: str
     debug_mode: bool = False
 
     model_config = SettingsConfigDict(env_file="../.env")
 
 settings = Settings()
-
+engine = create_engine(settings.database_url)
+SQLModel.metadata.create_all(engine)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
