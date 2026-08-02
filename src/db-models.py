341afca0f0af7dc0adcb46fc5b8180e6d8a2c16f
Kayda Seutter <kmseutter@gmail.com>
2026-08-01 20:09:20 -0600

    added models of tables

---
+++ src/db-models.py
@@ -0,0 +1,21 @@
+from sqlmodel import Field, SQLModel
+
+class Users(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    password: str | None = None
+    email: str | None = None
+    address: str | None = None
+    phone: str | None = None
+
+class Organizations(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+
+class Roles(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    role: str
+
+class Permissions(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    permission: str
